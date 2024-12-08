acfb742f7a058adb62bde13a4e56acb4fcd5c282
zhaleh <zhalehelmi@gmail.com>
2024-12-08 13:29:11 +0330

    customize component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const Home = () => {
   return (
-    <div>
-      <Button>Enter</Button>
+    <div className="m-4">
+      <Input />
+      <Button size="xs">Primary123</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 };
