bd5f15ff51cf0486e05ae43b4c5c84ff0af52435
zhaleh <zhalehelmi@gmail.com>
2024-12-28 17:40:58 +0330

    cookies

---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "cwm-jira-clone-session";
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,7 +1,11 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
+import { createAdminiClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { setCookie } from "hono/cookie";
 
 import { loginSchema, registerSchema } from "../schemas";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
   .post("/login", zValidator("json", loginSchema), (c) => {
@@ -14,9 +18,20 @@ const app = new Hono()
   .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } = c.req.valid("json");
 
-    console.log({ name, email, password });
+    const { account } = await createAdminiClient();
+    const user = await account.create(ID.unique(), email, password, name);
 
-    return c.json({ name, email, password });
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ data: user });
   });
 
 export default app;
