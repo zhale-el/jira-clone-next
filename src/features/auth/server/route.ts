50e257282ce9cf82ea07b9f2c6a4a7d4d5dfa054
zhaleh <zhalehelmi@gmail.com>
2024-12-28 18:11:28 +0330

    cookies for login

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -2,24 +2,32 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { createAdminiClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
-import { setCookie } from "hono/cookie";
+import { setCookie, deleteCookie } from "hono/cookie";
 
 import { loginSchema, registerSchema } from "../schemas";
 import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
-  .post("/login", zValidator("json", loginSchema), (c) => {
+  .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
-    console.log({ email, password });
+    const { account } = await createAdminiClient();
+    const session = await account.createEmailPasswordSession(email, password);
 
-    return c.json({ email, password });
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true });
   })
   .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } = c.req.valid("json");
 
     const { account } = await createAdminiClient();
-    const user = await account.create(ID.unique(), email, password, name);
 
     const session = await account.createEmailPasswordSession(email, password);
 
@@ -31,7 +39,12 @@ const app = new Hono()
       maxAge: 60 * 60 * 24 * 30,
     });
 
-    return c.json({ data: user });
+    return c.json({ success: true });
+  })
+  .post("/logout", (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+
+    return c.json({ success: true });
   });
 
 export default app;
