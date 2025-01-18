5c6d76bc261356c5541b4cbdfcae57cbef314d65
zhaleh <zhalehelmi@gmail.com>
2025-01-18 18:07:57 +0330

    workspace switcher

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { DottedSeparator } from "./dotted-separator";
 import { Navigaiton } from "./navigation";
+import { WorkspaceSwitcher } from "./workspace-switcher";
 
 export const Sidebar = () => {
   return (
@@ -9,6 +10,8 @@ export const Sidebar = () => {
       <Link href="/">
         <Image src="/logo.svg" alt="logo" width={164} height={48} />
       </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigaiton />
     </aside>
---
+++ src/components/workspace-switcher.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { RiAddCircleFill } from "react-icons/ri";
+
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export const WorkspaceSwitcher = () => {
+  const { data: workspaces } = useGetWorkspaces();
+
+  return (
+    <div className="flex flex-col gap-y-2">
+      <div className="flex items-center justify-between">
+        <p className="text-xs uppercase text-neutral-500">Workspaces</p>
+        <RiAddCircleFill
+          className="size-5 
+        text-neutral-500 cursor-pointer hover:opacity-75 transition"
+        />
+      </div>
+      <Select>
+        <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
+          <SelectValue placeholder="No workspace selected" />
+        </SelectTrigger>
+        <SelectContent>
+          {workspaces?.documents.map((workspace) => (
+            <SelectItem key={workspace.$id} value={workspace.$id}>
+              {workspace.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
