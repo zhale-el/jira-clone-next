3b4499e62393abe7dbd758ab948cc7a2a555cfc5
zhaleh <zhalehelmi@gmail.com>
2025-01-20 16:17:54 +0330

    workspace member

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -24,6 +24,7 @@ export const useLogout = () => {
       toast.success("Logged out");
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ["current"] });
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
     },
 
     onError: () => {
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,19 +1,39 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
+
+import { MemberRole } from "@/features/members/types";
 
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
+import {
+  DATABASE_ID,
+  IMAGES_BUCKET_ID,
+  MEMBERS_ID,
+  WORKSPACES_ID,
+} from "@/config";
 
 import { createWorkspaceSchema } from "../schemas";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
+    const user = c.get("user");
     const databases = c.get("databases");
+
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal("userId", user.$id),
+    ]);
+
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } });
+    }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
 
     const workspaces = await databases.listDocuments(
       DATABASE_ID,
-      WORKSPACES_ID
+      WORKSPACES_ID,
+
+      [Query.orderDesc("$createdAt"), Query.contains("$id", workspaceIds)]
     );
     return c.json({ data: workspaces });
   })
@@ -56,6 +76,12 @@ const app = new Hono()
           imageUrl: uploadedImageUrl,
         }
       );
+
+      await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+        userId: user.$id,
+        workspaceId: workspace.$id,
+        role: MemberRole.ADMIN,
+      });
 
       return c.json({ data: workspace });
     }
