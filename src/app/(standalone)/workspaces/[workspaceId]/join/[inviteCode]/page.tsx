8700ca61d54a73aa55c7498fe7757cbce9e97e7b
unknown <zhalehelmi@gmail.com>
2025-03-22 18:33:55 +0330

    invite ...

--- src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -1,5 +1,32 @@
-const WorkspaceJoinPage = () => {
-  return <div>workspace Id join page</div>;
+import { redirect } from "next/navigation";
+
+import { getCurrent } from "@/features/auth/queries";
+import { JoinWorkspaceForm } from "@/features/workspaces/components/join-workspace-form";
+import { getWorkspaceInfo } from "@/features/workspaces/queries";
+
+interface WorkspaceJoinPageProps {
+  params: {
+    workspaceId: string;
+  };
+}
+
+const WorkspaceJoinPage = async ({ params }: WorkspaceJoinPageProps) => {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+
+  const initialValues = await getWorkspaceInfo({
+    workspaceId: params.workspaceId,
+  });
+
+  if (!initialValues) {
+    redirect("/");
+  }
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <JoinWorkspaceForm initialValues={initialValues} />
+    </div>
+  );
 };
 
 export default WorkspaceJoinPage;
---
+++ src/features/workspaces/components/join-workspace-form.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+import { useJoinWorkspace } from "../api/use-join-workspace";
+
+interface JoinWorkspaceFormProps {
+  initialValues: {
+    name: string;
+  };
+}
+
+export const JoinWorkspaceForm = ({
+  initialValues,
+}: JoinWorkspaceFormProps) => {
+  const { mutate } = useJoinWorkspace();
+
+  return (
+    <Card className="w-full h-full border-none shadow-none">
+      <CardHeader className="p-7">
+        <CardTitle className="text-xl font-bold">Join workspace</CardTitle>
+        <CardDescription>
+          You&apos;ve been invited to join <strong>{initialValues.name}</strong>{" "}
+          workspace
+        </CardDescription>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <div className="flex flex-col lg:flex-row gap-2 items-center justify-between">
+          <Button
+            variant="secondary"
+            type="button"
+            size="lg"
+            asChild
+            className="w-full lg:w-fit"
+          >
+            <Link href="/"> Cancel</Link>
+          </Button>
+          <Button type="button" size="lg" className="w-full lg:w-fit">
+            Join Workspace
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
