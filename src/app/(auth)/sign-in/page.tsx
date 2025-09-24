2557680707288766419d6a03e466a552f8e86a7c
zhale-el <zhalehelmi@gmail.com>
2025-09-24 19:28:13 +0330

    fixed project-name-apprwrite

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -6,8 +6,6 @@ import { SignInCard } from "@/features/auth/components/sign-in-card";
 const SignInPage = async () => {
   const user = await getCurrent();
 
-  console.log({ user });
-
   if (user) redirect("/");
 
   return <SignInCard />;
